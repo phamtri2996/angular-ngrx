b3894863ba9aff5f30ae21b90902d0e1c6009b74
tri.phamsy <pham.stri@gmail.com>
2024-02-05 12:20:44 +0700

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from "./counter/counter/counter.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, CounterComponent]
+})
+export class AppComponent {
+  title = 'test';
+}
